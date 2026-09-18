f99d36673c2ae0a7d111bd70018948eda95b1076
Stepanchikkk <stepan3045@mail.ru>
2026-09-18 14:00:46 +0500

    update twitch-modpanel.user.js

--- twitch-modpanel.user.js
+++ twitch-modpanel.user.js
@@ -2,7 +2,7 @@
 // @name            Twitch ModPanel
 // @namespace       TMP
 // @description     Панель модератора для Twitch
-// @version         0.1.3.0
+// @version         0.1.4.0
 // @author          Twitch ModPanel Team
 //
 // @grant           GM_getValue
